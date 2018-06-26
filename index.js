814b75a4100e1018350eaa7365471ecccec2c398
Lisa9740 <barretalison@gmail.co>
2018-06-26 09:02:37 +0400

    Ajout de la variable prenom qui s'affiche dans le fichier ejs avec une fonction

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     // création de la variable prenom
     var prenom = {prenom : "Alison"};
-      res.render('index', prenom );
+   
+    get_prenom(function(prenom){
+        //console.log(hotels); 
+    })
+    res.render('index', {
+        prenom: prenom
+    });
+    //   res.render('index', prenom );
   });
+
+  function get_prenom(cb,result){
+
+  cb (result);
+}
+
   
   app.listen(port, function(){
     console.log('the port is on')
